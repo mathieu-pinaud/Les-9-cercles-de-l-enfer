2fc507204aa8cd6d5facf5d5a035159376e9ff04
salvatore-anania <salvatore.anania@laplateforme.io>
2024-04-23 15:18:13 +0200

    modif client

--- client/main.cpp
+++ client/main.cpp
@@ -2,9 +2,15 @@
 #include <iostream>
 #include <cstring> // Pour std::strlen
 
-int main() {
+int main(int argc, char** __argv){
+    if(argc != 3){
+        std::cerr << "Usage: " << __argv[0] << " <ip> <port>" << std::endl;
+        return 1;
+    }
+    char* ip = __argv[1];
+    int port = atoi(__argv[2]);
     LPTF_Socket clientSocket;
-    if (clientSocket.connectTo("10.10.3.4", 12345)) { // Remplacer les paramètres d'adresse et de port par les valeurs appropriées
+    if (clientSocket.connectTo(ip, port)) { // Remplacer les paramètres d'adresse et de port par les valeurs appropriées
         std::cout << "Connecté au serveur." << std::endl;
 
         while (true) {
