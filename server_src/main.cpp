b48d38fd5a68bd9de6ef6fe640f149b2926f2e44
mathieu <mathieu.pinaud@laplateofrme.io>
2024-08-26 11:12:30 +0200

    commit caca

--- server_src/main.cpp
+++ server_src/main.cpp
@@ -2,7 +2,24 @@
 #include "../window/window.hpp"
 #include <vector>
 
-
+int static checkArgs(int ac, char* av[]) {
+    if (ac != 2) {
+        std::cerr << "Usage: " << av[0] << " <port>" << std::endl;
+        return -1;
+    }
+    int port = -1;
+    try {
+        port = std::stoi(av[1]);
+    } catch (const std::exception& e) {
+        std::cerr << "Le port doit être un entier." << std::endl;
+        return -1;
+    }
+    if (port < 1024 || port > 49151) {
+        std::cerr << "Le port doit être compris entre 1024 et 49151." << std::endl;
+        return -1;
+    }
+    return port;
+}
 
 
 int main(int ac, char* av[]) {
